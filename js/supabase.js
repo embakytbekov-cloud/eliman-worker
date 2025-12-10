2b26ee0f100938762dd4ea09ecf65754569c41b2
em <embakytbekov@gmail.com>
2025-12-10 17:06:20 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = "https://ccqldccmikwdjkbxmcsn.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjcWxkY2NtaWt3ZGprYnhtY3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMzIxODEsImV4cCI6MjA4MDcwODE4MX0.ce7CPoACHTU6ryGjoELPywa1rpGmDKG5TIZxPFbleuA";
 
-const { createClient } = supabase;
 
 window.db = createClient(SUPABASE_URL, SUPABASE_KEY);
