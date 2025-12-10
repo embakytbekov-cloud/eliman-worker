31ecf9ec1c2afa6c275cd5e263d7284db2ca67df
em <embakytbekov@gmail.com>
2025-12-10 17:15:31 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,6 @@
+// 🔥 Supabase credentials
 const SUPABASE_URL = "https://ccqldccmikwdjkbxmcsn.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjcWxkY2NtaWt3ZGprYnhtY3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMzIxODEsImV4cCI6MjA4MDcwODE4MX0.ce7CPoACHTU6ryGjoELPywa1rpGmDKG5TIZxPFbleuA";
 
-
-window.db = createClient(SUPABASE_URL, SUPABASE_KEY);
+// 🔥 Здесь создаём клиент Supabase ПРАВИЛЬНО
+window.db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
