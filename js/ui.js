40c3ffad294f06213c86b61bdd1ea4b906e94694
em <embakytbekov@gmail.com>
2025-12-09 19:26:29 -0600

    Delete js/ui.js

--- js/ui.js
+++
@@ -1,9 +0,0 @@
-export function showMainApp(worker) {
-  document.getElementById("registerScreen").classList.add("hidden");
-  document.getElementById("mainScreen").classList.remove("hidden");
-
-  window.currentWorker = worker;
-
-  // Optional: обновить профиль
-  // updateProfileUI(worker);
-}
