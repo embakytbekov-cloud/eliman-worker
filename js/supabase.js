1d7c20acbecc1e8ec3cda171fd4fa040aff583a2
em <embakytbekov@gmail.com>
2025-12-10 15:24:01 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://ccqldccmikwdjkbxmcsn.supabase.co";
-const SUPABASE_KEY = "ТВОЙ_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjcWxkY2NtaWt3ZGprYnhtY3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMzIxODEsImV4cCI6MjA4MDcwODE4MX0.ce7CPoACHTU6ryGjoELPywa1rpGmDKG5TIZxPFbleuA";
 
 const { createClient } = supabase;
 
